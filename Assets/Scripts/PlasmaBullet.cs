d5ca4880dbf86fa31b92ec76bb1137a8decf9999
Aum <imaum2208@gmail.com>
2021-05-13 14:43:36 +0100

    plasma bullet collision and decal test

--- Assets/Scripts/PlasmaBullet.cs
+++ Assets/Scripts/PlasmaBullet.cs
@@ -4,12 +4,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlasmaBullet : MonoBehaviour {
-    public const string NAME = "PlasmaBullet";
-    [SerializeField] private float bulletSpeed = 1f;
+    public const             string NAME        = "PlasmaBullet";
+    [SerializeField] private float  bulletSpeed = 1f;
 
     // lifetime in seconds
-    public float lifetime = 10f;
+    public  float lifetime      = 10f;
     private float lifetimeTimer = 0f;
+
+    public bool debug = false;
 
     // Start is called before the first frame update
     void Start() {
@@ -17,14 +19,37 @@ public class PlasmaBullet : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        transform.position += transform.forward * (bulletSpeed * Time.deltaTime);
-        lifetimeTimer -= Time.deltaTime;
-        if (lifetimeTimer < 0) {
-            PoolingManager.Despawn(NAME, gameObject);
+        if (!debug) {
+            transform.position += transform.forward * (bulletSpeed * Time.deltaTime);
+            lifetimeTimer -= Time.deltaTime;
+            if (lifetimeTimer < 0) {
+                Despawn();
+            }
         }
     }
 
     private void OnEnable() {
         lifetimeTimer = lifetime;
+    }
+
+    // on collision
+    private void OnTriggerEnter(Collider other) {
+        // despawn
+        Despawn();
+        Debug.Log("ST");
+        // apply damage
+        
+    }
+    
+    private void OnCollisionEnter(Collision other) {
+        // despawn
+        Despawn();
+        Debug.Log("DT");
+        // apply damage
+    }
+
+
+    public void Despawn() {
+        PoolingManager.Despawn(NAME, gameObject);
     }
 }
